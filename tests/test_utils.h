30d56d4eb0c2cc77082017e586f1482437de2248
Chris Tsantiris <chris.tsantiris@gmail.com>
2026-04-01 13:58:17 -0400

    Color code test results

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -6,19 +6,28 @@
 extern int tests_run;
 extern int tests_passed;
 
-#define ASSERT(msg, condition) do {         \
-    tests_run++;                            \
-    if (condition) {                        \
-        tests_passed++;                     \
-        printf("  PASS: %s\n", msg);        \
-    } else {                                \
-        printf("  FAIL: %s\n", msg);        \
-    }                                       \
+#define ANSI_GREEN "\033[32m"
+#define ANSI_RED   "\033[31m"
+#define ANSI_RESET "\033[0m"
+
+#define ASSERT(msg, condition) do {                                          \
+    tests_run++;                                                             \
+    if (condition) {                                                         \
+        tests_passed++;                                                      \
+        printf(ANSI_GREEN "  PASS: %s\n" ANSI_RESET, msg);                  \
+    } else {                                                                 \
+        printf(ANSI_RED   "  FAIL: %s\n" ANSI_RESET, msg);                  \
+    }                                                                        \
 } while(0)
 
-#define REPORT() do {                                           \
-    printf("\n%d/%d tests passed\n", tests_passed, tests_run); \
-    return tests_passed == tests_run ? 0 : 1;                  \
+#define REPORT() do {                                                        \
+    if (tests_passed == tests_run) {                                         \
+        printf(ANSI_GREEN "\n%d/%d tests passed\n" ANSI_RESET,              \
+            tests_passed, tests_run);                                        \
+    } else {                                                                 \
+        printf(ANSI_RED "\n%d/%d tests passed\n" ANSI_RESET,                \
+            tests_passed, tests_run);                                        \
+    }                                                                        \
 } while(0)
 
 #endif
