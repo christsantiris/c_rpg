9b2cc136721e2133811100ba6eaccb4efa663b92
Chris Tsantiris <chris.tsantiris@gmail.com>
2026-04-02 13:25:53 -0400

    Fix bottom bar spacing

--- src/renderer/message_bar.c
+++ src/renderer/message_bar.c
@@ -2,15 +2,15 @@
 
 void message_bar_draw(Renderer *r, const GameState *g) {
     // Background bar
-    SDL_Rect bar = { 0, r->screen_h - MESSAGE_BAR_H,
-                     r->screen_w, MESSAGE_BAR_H };
+    int bar_top = r->tiles_y * TILE_SIZE;
+    SDL_Rect bar = { 0, bar_top,
+                     r->screen_w, r->screen_h - bar_top };
     SDL_SetRenderDrawColor(r->sdl, 10, 10, 20, 255);
     SDL_RenderFillRect(r->sdl, &bar);
 
     // Top border line
     SDL_SetRenderDrawColor(r->sdl, 58, 58, 106, 255);
-    SDL_RenderDrawLine(r->sdl, 0, r->screen_h - MESSAGE_BAR_H,
-                       r->screen_w, r->screen_h - MESSAGE_BAR_H);
+    SDL_RenderDrawLine(r->sdl, 0, bar_top, r->screen_w, bar_top);
 
     if (g->message_count == 0) return;
 
